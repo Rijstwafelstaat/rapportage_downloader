eaa863996ede356f3cf39d353a6d2b4b7b9dee27
Michael Scholten <michael-scholten@hotmail.nl>
2023-11-30 12:05:03 +0100

    Made arguments optional to make it easier to use on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
-use std::{path::PathBuf, str::FromStr};
+use std::{
+    io::{self, Write as _},
+    path::PathBuf,
+    str::FromStr as _,
+};
 
-use clap::Parser;
+use clap::{Parser, ValueEnum as _};
 use login::CookieStore;
 use report::Report;
 use reqwest::Client;
 use tokio::{
     fs::{self, File},
-    io::AsyncWriteExt,
+    io::AsyncWriteExt as _,
 };
 
 mod login;
@@ -16,24 +20,24 @@ mod report;
 pub struct Args {
     /// The email to use to login at DB Energie
     #[arg(short, long)]
-    mail: String,
+    mail: Option<String>,
 
     /// The password to use to login at DB Energie
     #[arg(short, long)]
-    password: String,
+    password: Option<String>,
 
     /// The rapport to download
     #[arg(short, long)]
-    report: Report,
+    report: Option<Report>,
 
     /// The directory path or url to write the received message to
     #[arg(short, long)]
-    output: String,
+    output: Option<String>,
 }
 
 /// Saves the data to a server or file
-async fn save(client: &Client, args: &Args, data: Vec<u8>, filename: String) {
-    if let Ok(url) = url::Url::from_str(&args.output) {
+async fn save(client: &Client, output: &str, data: Vec<u8>, filename: String) {
+    if let Ok(url) = url::Url::from_str(output) {
         // Store the report in a form
         let form = reqwest::multipart::Form::new().part(
             "file",
@@ -47,7 +51,7 @@ async fn save(client: &Client, args: &Args, data: Vec<u8>, filename: String) {
             .send()
             .await
             .expect("Failed to send file");
-    } else if let Ok(directory) = PathBuf::from_str(&args.output) {
+    } else if let Ok(directory) = PathBuf::from_str(output) {
         // Create the requested directory
         fs::create_dir_all(&directory)
             .await
@@ -68,10 +72,38 @@ async fn save(client: &Client, args: &Args, data: Vec<u8>, filename: String) {
     }
 }
 
+fn read_line(request: &str) -> String {
+    // Write the request to the screen
+    io::stdout()
+        .write_all(request.as_bytes())
+        .expect("Failed to write output");
+
+    // Read the data from the screen
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read mail");
+
+    // Trim and return the data
+    line.trim().to_owned()
+}
+
 #[tokio::main]
 async fn main() {
     // Parse the arguments
     let args = Args::parse();
+
+    // Unpack the arguments, read them from the terminal if None
+    let mail = args.mail.unwrap_or_else(|| read_line("Enter mail: "));
+    let password = args
+        .password
+        .unwrap_or_else(|| read_line("Enter password: "));
+    let report = args.report.unwrap_or_else(|| {
+        Report::from_str(&read_line("Report types: aansluitinglijst, belastingcluster, co2, datakwaliteit, gebouwen, meet-en-infra, metadata, meterstanden, mj, tussenmeter, verbruik\nEnter report type: "), true).expect("Invalid report type")
+    });
+    let output = args
+        .output
+        .unwrap_or_else(|| read_line("Enter the output path: "));
 
     // Create a client
     let client = Client::builder()
@@ -80,17 +112,23 @@ async fn main() {
         .expect("Failed to create client");
 
     // Log in to receive a cookie
-    let cookie_store = CookieStore::login(&args.mail, &args.password)
+    let cookie_store = CookieStore::login(&mail, &password)
         .await
         .expect("Login failed");
 
     // Download the latest version of the requested report
-    let (filename, response) = args
-        .report
+    let (filename, response) = report
         .download_latest_version(&cookie_store)
         .await
         .expect("Failed to download latest report version");
 
-    // Save the response to a server or file
-    save(&client, &args, response, filename).await;
+    if output.is_empty() {
+        println!(
+            "{}",
+            core::str::from_utf8(&response).expect("Response wasn't valid utf-8")
+        );
+    } else {
+        // Save the response to a server or file
+        save(&client, &output, response, filename).await;
+    }
 }
